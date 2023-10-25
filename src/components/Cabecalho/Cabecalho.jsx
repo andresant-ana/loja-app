90f71777b418f805abb449f8969f0faa9288011e
andresant-ana <rm551575@fiap.com.br>
2023-10-25 09:58:21 -0300

    Estilizando o cabeçalho.

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -2,13 +2,13 @@ import Link from 'next/link'
 
 export default function Cabecalho() {
   return (
-    <header className='bg-slate-700 h-30 text-white'>
+    <header className='cabecalho'>
         <nav>
             <ul>
-                <li><Link href="/">→ Home</Link></li>
-                <li><Link href="/produtos/camisa">→ Camisa</Link></li>
-                <li><Link href="/produtos/tenis">→ Tenis</Link></li>
-                <li><Link href="/fazenda">→ Queijos</Link></li>
+                <li><Link href="/">HOME</Link></li>
+                <li><Link href="/produtos/camisa">CAMISA</Link></li>
+                <li><Link href="/produtos/tenis">TÊNIS</Link></li>
+                <li><Link href="/fazenda">QUEIJOS</Link></li>
             </ul>
         </nav>
     </header>
